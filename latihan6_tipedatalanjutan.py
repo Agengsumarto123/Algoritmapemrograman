56c906803768e1ad797797f612445280c714c974
Agengsumarto123 <136145747+Agengsumarto123@users.noreply.github.com>
2023-07-01 19:00:48 +0700

    Update latihan6_tipedatalanjutan.py

--- latihan6_tipedatalanjutan.py
+++ latihan6_tipedatalanjutan.py
@@ -25,4 +25,4 @@ print ("nilai luas lingkaran adalah : %d" %luas)
 # menyusun kata
 print ("============================================================")
 kata = "mahasiswa","UNISS", "Batang", "semester", 2, "belajar","dengan", "topik", "algoritma", "pemrograman", "dan", "tipe", "data", "di","lab"
-print ("menyusun kata = %s"%kata[0],kata[1],kata[2],kata[3],kata[4],kata[5],kata[6],kata[7],kata[8],kata[9],kata[10],kata[11],kata[12],kata[13],kata[14],kata[15])
+print ("menyusun kata = %s"%kata[0],kata[1],kata[2],kata[3],kata[4],kata[5],kata[6],kata[7],kata[8],kata[9],kata[10],kata[11],kata[12],kata[13],kata[14])
