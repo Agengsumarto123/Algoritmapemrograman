7607b0d37a78f0be29918e9c35ee0d7a665cca6a
Agengsumarto123 <136145747+Agengsumarto123@users.noreply.github.com>
2023-07-01 13:12:14 +0700

    Create latihan6_tipedatalanjutan.py

---
+++ latihan6_tipedatalanjutan.py
@@ -0,0 +1,28 @@
+#identity
+a = ["2023"]
+b = ["ageng"]
+c = ["ti2A"]
+d = ["informatika"]
+
+# hitung
+luas=3.14*7*7
+
+# 
+print ("============================================================")
+print ("    OPERASI ARITMATIKA ")
+print ("============================================================")
+print ("nim =%s "%(a [0]))
+print ("nama =%s "%(b [0]))
+print ("kelas =%s "%(c [0]))
+print ("prodi =%s "%(d [0]))
+print ("============================================================")
+print ("    LINGKARAN   ")
+print ("============================================================")
+print ("nilai luas lingkaran adalah : %d" %luas)
+
+
+
+# menyusun kata
+print ("============================================================")
+kata = "mahasiswa","UNISS", "Batang", "semester", 2, "belajar","dengan", "topik", "algoritma", "pemrograman", "dan", "tipe", "data", "di","lab"
+print ("menyusun kata = %s"%kata[0],kata[1],kata[2],kata[3],kata[4],kata[5],kata[6],kata[7],kata[8],kata[9],kata[10],kata[11],kata[12],kata[13],kata[14],kata[15])
